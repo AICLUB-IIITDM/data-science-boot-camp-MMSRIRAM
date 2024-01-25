1af6cc2c77a7226f1c815eb63cb84c88971efeb9
Sriram <mmsriram3066@gmail.com>
2024-01-25 21:36:51 +0530

    Update Math.py

--- day01/Math.py
+++ day01/Math.py
@@ -1,3 +1,7 @@
 # write a function to implement addition
 def add(a,b):
-    pass
+    c=a+b
+    print("The sum is",c)
+a=int(input("Enter the number 1:"))
+b=int(input("Enter the number 2:"))
+add(a,b)
